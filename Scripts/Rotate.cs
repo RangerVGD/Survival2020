2c99f515dab6d597f1120e6db3117718ff1f461c
RangerVGD <nathan.nunley@comalisd.org>
2020-12-09 11:38:54 -0600

    Add files via upload

---
+++ Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour 
+{
+
+	public int XRotation;
+	public int YRotation;
+	public int ZRotation;
+
+	void Update () 
+	{
+		transform.Rotate (new Vector3 (XRotation, YRotation, ZRotation) * Time.deltaTime);
+	}
+}
